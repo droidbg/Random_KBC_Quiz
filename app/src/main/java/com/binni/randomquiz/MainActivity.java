30ed4ba97c8aa5d92057fb7e12bf4eba510aacd0
Binni <binni1108@gmail.com>
2021-09-16 09:13:54 +0530

    Initial UI Commit

--- app/src/main/java/com/binni/randomquiz/MainActivity.java
+++ app/src/main/java/com/binni/randomquiz/MainActivity.java
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 Button goButton;
-void start(View view)
-{
-    goButton.setVisibility(view.INVISIBLE);
-}
+int nooflifeline=3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         goButton=findViewById(R.id.goButton);
+    }
+    void start(View view)
+    {
+        goButton.setVisibility(view.INVISIBLE);
+    }
+    public void chooseAnswer(View view) {
+    }
+    public void lifelineYesNo(View view) {
+       if(view.getTag().toString().equals("no")){
+           Toast.makeText(this, "OK!! Your choice./n But Hey!! You have Still "+nooflifeline+" Remaining", Toast.LENGTH_SHORT).show();
+       }
     }
 }
